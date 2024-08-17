a3cf775092fc354e6becc306bb6b06a8b0066205
tifiouse-py <tifiouse.tfs@gmail.com>
2024-08-17 15:58:04 +0200

    First Upload

---
+++ docs/README.md
@@ -0,0 +1,43 @@
+# Nations Of World Launcher 🚀
+
+## Overview
+Welcome to the Nations Of Word Launcher, a multi-project launcher for Minecraft developed by Nations Of World. This launcher is designed to provide seamless access to various Minecraft projects developed by our studio.
+
+## Features
+- **Multi-project Support:** Access multiple Minecraft projects developed by Nations Of World from a single launcher.
+- **Easy Installation:** Simple installation process to get you started quickly.
+- **Automatic Updates:** Keep your projects up-to-date with automatic updates.
+- **Customizable Settings:** Tailor your gaming experience with customizable settings.
+
+## Installation
+1. 📥 Download the latest version of the Nations Of World Launcher from our official website.
+2. ⚙️ Run the installer and follow the on-screen instructions.
+3. 🚀 Once installed, launch the Nations Of World Launcher.
+4. 👤 Log in with your Nations Of World account or create a new one.
+5. 🔍 Browse and select the project you want to play.
+6. ▶️ Click "Play" to launch the selected project !
+
+## Updating
+The Nations Of World Launcher automatically checks for updates each time it is launched. If a new version is available, it will prompt you to update. Simply follow the on-screen instructions to update to the latest version.
+
+## Customization
+The launcher allows you to customize various settings to enhance your gaming experience. You can adjust graphics settings, control audio options, and more from the settings menu.
+
+## Support
+For any inquiries or assistance regarding the Nations Of World Launcher, please contact our support team at support@NationsOfWord.com.
+
+## Contributing
+We welcome contributions from the community to improve the Nations Of World Launcher. If you encounter any bugs or have suggestions for new features, please submit them through our GitHub repository.
+
+## License
+The Nations Of World Launcher is licensed under the MIT License. See the LICENSE file for details.
+
+## Acknowledgments
+We would like to thank the Minecraft community for their ongoing support and inspiration. Special thanks to our dedicated team at Nations Of World for their hard work in developing this launcher.
+
+## Connect with Us
+- Website: [www.starbytestudio.com](https://www.NationsOfWord.com)
+- Twitter: [@StarbyteStudio](https://twitter.com/StarbyteStudio)
+- Facebook: [Nations Of World](https://www.facebook.com/StarbyteStudio)
+
+Enjoy your gaming experience with Nations Of World Launcher! 🎮
---
+++ src/assets/js/index.js
@@ -0,0 +1,129 @@
+/**
+ * @author Tifiouse
+ * @license CC-BY-NC 4.0 - https://creativecommons.org/licenses/by-nc/4.0
+ */
+const { ipcRenderer } = require('electron');
+const os = require('os');
+import { config, database } from './utils.js';
+
+let dev = process.env.NODE_ENV === 'dev';
+
+
+class Splash {
+    constructor() {
+        this.splash = document.querySelector(".splash");
+        this.splashMessage = document.querySelector(".splash-message");
+        this.splashAuthor = document.querySelector(".splash-author");
+        this.message = document.querySelector(".message");
+        this.progress = document.querySelector(".progress");
+        document.addEventListener('DOMContentLoaded', async () => {
+            let databaseLauncher = new database();
+            let configClient = await databaseLauncher.readData('configClient');
+            let theme = configClient?.launcher_config?.theme || "auto"
+            let isDarkTheme = await ipcRenderer.invoke('is-dark-theme', theme).then(res => res)
+            document.body.className = isDarkTheme ? 'dark global' : 'light global';
+            if (process.platform == 'win32') ipcRenderer.send('update-window-progress-load')
+            this.startAnimation()
+        });
+    }
+
+    async startAnimation() {
+        let splashes = [
+            { "message": "Bienvenue !", "author": "NationsOfWord" },
+            { "message": "Bienvenue !", "author": "NationsOfWord" },
+            { "message": "Bienvenue !", "author": "NationsOfWord" }
+        ];
+        let splash = splashes[Math.floor(Math.random() * splashes.length)];
+        this.splashMessage.textContent = splash.message;
+        this.splashAuthor.children[0].textContent = "@" + splash.author;
+        await sleep(100);
+        document.querySelector("#splash").style.display = "block";
+        await sleep(500);
+        this.splash.classList.add("opacity");
+        await sleep(500);
+        this.splash.classList.add("translate");
+        this.splashMessage.classList.add("opacity");
+        this.splashAuthor.classList.add("opacity");
+        this.message.classList.add("opacity");
+        await sleep(1000);
+        this.checkUpdate();
+    }
+
+    async checkUpdate() {
+        if (dev) return this.startLauncher();
+        this.setStatus(`Recherche de mise à jour...`);
+
+        ipcRenderer.invoke('update-app').then().catch(err => {
+            return this.shutdown(`erreur lors de la recherche de mise à jour :<br>${err.message}`);
+        });
+
+        ipcRenderer.on('updateAvailable', () => {
+            this.setStatus(`Mise à jour disponible !`);
+            ipcRenderer.send('start-update');
+        })
+
+        ipcRenderer.on('error', (event, err) => {
+            if (err) return this.shutdown(`${err.message}`);
+        })
+
+        ipcRenderer.on('download-progress', (event, progress) => {
+            this.toggleProgress();
+            ipcRenderer.send('update-window-progress', { progress: progress.transferred, size: progress.total })
+            this.setProgress(progress.transferred, progress.total);
+        })
+
+        ipcRenderer.on('update-not-available', () => {
+            console.error("Mise à jour non disponible");
+            this.maintenanceCheck();
+        })
+    }
+
+    async maintenanceCheck() {
+        config.GetConfig().then(res => {
+            if (res.maintenance) return this.shutdown(res.maintenance_message);
+            this.startLauncher();
+        }).catch(e => {
+            console.error(e);
+            return this.shutdown("Aucune connexion internet détectée,<br>veuillez réessayer ultérieurement.");
+        })
+    }
+
+    startLauncher() {
+        this.setStatus(`Démarrage du launcher`);
+        ipcRenderer.send('main-window-open');
+        ipcRenderer.send('update-window-close');
+    }
+
+    shutdown(text) {
+        this.setStatus(`${text}<br>Arrêt dans 5s`);
+        let i = 4;
+        setInterval(() => {
+            this.setStatus(`${text}<br>Arrêt dans ${i--}s`);
+            if (i < 0) ipcRenderer.send('update-window-close');
+        }, 1000);
+    }
+
+    setStatus(text) {
+        this.message.innerHTML = text;
+    }
+
+    toggleProgress() {
+        if (this.progress.classList.toggle("show")) this.setProgress(0, 1);
+    }
+
+    setProgress(value, max) {
+        this.progress.value = value;
+        this.progress.max = max;
+    }
+}
+
+function sleep(ms) {
+    return new Promise(r => setTimeout(r, ms));
+}
+
+document.addEventListener("keydown", (e) => {
+    if (e.ctrlKey && e.shiftKey && e.keyCode == 73 || e.keyCode == 123) {
+        ipcRenderer.send("update-window-dev-tools");
+    }
+})
+new Splash();
